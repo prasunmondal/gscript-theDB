e6f5122178133d393ff5107fd3a7e119991d1d89
prasunmondal <prasun.mondal02@gmail.com>
2024-06-07 19:41:49 +0530

    fix

--- DELETE_ALL.js
+++ DELETE_ALL.js
@@ -4,6 +4,13 @@ function delete_all(sheetId, tabName) {
   var rows = sheet.getDataRange();
   var numOfRows = rows.getNumRows();
 
+  if(numOfRows == 1) {
+    return {
+      "statusCode": 204,
+      "content": "SUCCESS: 0 row(s) deleted",
+      "rowsAffected": 0
+    }
+  }
   var lock = LockService.getScriptLock();
   sheet.deleteRows(2, numOfRows - 1);
   lock.releaseLock()
