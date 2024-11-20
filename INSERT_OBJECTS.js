4ff3991513b87d6f6e761dba14814fdc0c3400d6
Prasun Mondal <prasun.mondal02@gmail.com>
2024-11-20 23:24:22 +0530

    Revert "improve insert_objects performance"
    This reverts commit fc6324983484bdd6fb71c1593fcc653c3ea6c3d9.

--- INSERT_OBJECTS.js
+++ INSERT_OBJECTS.js
@@ -1,38 +1,32 @@
 function insert_objects(jsonObj) {
   var sheetId = jsonObj.sheetId;
   var tabName = jsonObj.tabName;
-  var dataList = JSON.parse(jsonObj.objectData);
+  var dataList = JSON.parse(jsonObj.objectData)
 
   var ss = SpreadsheetApp.openById(sheetId);
-  var sheet = ss.getSheetByName(tabName);
-
-  // Get headers and map columns to keys only once
   var headers = getHeaderRow_(ss, tabName);
   var keys = Object.keys(dataList[0]);
-  var colMap = getColumnMap(keys, headers);
+  var colMap = getColumnMap(keys, headers)
+  var sheet = ss.getSheetByName(tabName);
 
-  // Prepare rows to insert
-  var rowsToAdd = dataList.map(function(record) {
-    return keys.map(function(key) {
-      var colIndex = colMap[key];  // Column index from the map
-      return record[key] || '';  // Default to empty string if value is undefined
-    });
-  });
-
-  // Get the last row and use it to set the range
-  var lastRow = sheet.getLastRow();
-  if (rowsToAdd.length > 0) {
-    // Perform batch insert for all rows in one go
-    sheet.getRange(lastRow + 1, 1, rowsToAdd.length, rowsToAdd[0].length).setValues(rowsToAdd);
+  var rowsToAdd = []
+  for (var k = 0; k < dataList.length; k++) {
+    var rowData = []
+    var values = Object.values(dataList[k]);
+    for (var i = 0; i < values.length; i++) {
+      rowData[colMap[i]] = JSON.stringify(values[i]).replace('"', '').replace('"', '');
+    }
+    rowsToAdd.push(rowData)
   }
+  var lastRow = sheet.getLastRow();
+  sheet.getRange(lastRow + 1, 1, rowsToAdd.length, rowsToAdd[0].length).setValues(rowsToAdd);
 
   return {
     "statusCode": 200,
     "content": "Inserted Successfully",
     "rowsAffected": dataList.length
-  };
+  }
 }
-
 
 const cachedColMap = {}
 function getColumnMap(keys, headers, sheetId, tabname) {
